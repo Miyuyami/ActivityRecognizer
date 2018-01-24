b5259b016ea511a7bb6eb679f7fa09699a791f11
Miyuyami <tenshi.tenshi.angel@gmail.com>
2018-01-24 13:24:57 +0100

    app.config with runtime binding

--- ActivityRecognizer/Plugin.ActivityRecognizer/Properties/AssemblyInfo.cs
+++ ActivityRecognizer/Plugin.ActivityRecognizer/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Plugin.ActivityRecognizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cross platform activity recognition")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Plugin.ActivityRecognizer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright (C)  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
